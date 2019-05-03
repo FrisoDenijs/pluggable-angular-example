7d22f37c210d6084b2da47c20cf35da94e9f5b68
FrisoDenijs <friso1990@gmail.com>
2019-05-03 13:01:02 +0200

    abdul's suggestion for polyfill, still not working

--- base/src/polyfills.ts
+++ base/src/polyfills.ts
@@ -73,8 +73,9 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'systemjs';
+
+(window as any).global = window;
