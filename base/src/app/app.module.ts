4472560da17b69c13809be931f6966d9254d10d1
FrisoDenijs <friso1990@gmail.com>
2019-05-03 02:08:34 +0200

    attempted fixes for systemjs, still not working

--- base/src/app/app.module.ts
+++ base/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { COMPILER_OPTIONS, CompilerFactory, Compiler } from '@angular/core';
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
+import { DashboardService } from './shared';
+import { CommonModule } from '@angular/common';
+
+export function createCompiler(fn: CompilerFactory): Compiler {
+  return fn.createCompiler();
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +20,28 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    DashboardService,
+    {
+      provide: COMPILER_OPTIONS,
+      useValue: {},
+      multi: true
+    },
+    {
+      provide: CompilerFactory,
+      useClass: JitCompilerFactory,
+      deps: [COMPILER_OPTIONS]
+    },
+    {
+      provide: Compiler,
+      useFactory: createCompiler,
+      deps: [CompilerFactory]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
