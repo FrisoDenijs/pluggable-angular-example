d28855a16b790fab4683a8e27f00d2b45799c521
FrisoDenijs <friso1990@gmail.com>
2019-05-05 22:58:50 +0200

    setup for systemjs

--- core/src/app/app.module.ts
+++ core/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { COMPILER_OPTIONS, CompilerFactory, Compiler, NgModule } from '@angular/core';
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
+
+export function createCompiler(fn: CompilerFactory): Compiler {
+  return fn.createCompiler();
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +15,21 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: COMPILER_OPTIONS,
+    useValue: {},
+    multi: true
+  },
+  {
+    provide: CompilerFactory,
+    useClass: JitCompilerFactory,
+    deps: [COMPILER_OPTIONS]
+  },
+  {
+    provide: Compiler,
+    useFactory: createCompiler,
+    deps: [CompilerFactory]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
